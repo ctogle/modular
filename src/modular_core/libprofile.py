c3d7120e0a3ad75fa6fa5d60b53c63c0719af8a9
ctogle <cogle@vt.edu>
2014-08-14 15:18:53 -0400

    added libprofile

---
+++ src/modular_core/libprofile.py
@@ -0,0 +1,27 @@
+import modular_core.libfundamental as lfu
+
+import pstats, cProfile
+import cPickle as pickle
+
+import os, traceback, sys
+
+import pdb
+
+def profile_function(func_):
+	#cProfile.runctx('lchem.simulate()',
+	cProfile.runctx('func_()',globals(),locals(),'profile.prof')
+	s = pstats.Stats('profile.prof')
+	s.strip_dirs().sort_stats('time').print_stats()
+	os.remove('profile.prof')
+	pdb.set_trace()
+
+if __name__ == '__main__':
+	print 'this is a library!'
+
+if __name__ == 'libs.modular_core.libprofile':
+	if lfu.gui_pack is None: lfu.find_gui_pack()
+	lgm = lfu.gui_pack.lgm
+	lgd = lfu.gui_pack.lgd
+	lgb = lfu.gui_pack.lgb
+
+
