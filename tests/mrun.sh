e577a537fec736d98b8171be5545da557c28ac71
ctogle <cogle@vt.edu>
2015-04-21 23:10:58 -0400

    check

--- tests/mrun.sh
+++ tests/mrun.sh
@@ -1,8 +1,8 @@
 
 echo "performing mpirun of modular!"
-#mpirun --nooversubscribe --hostfile ~/dev/hostfile modular.py correl_demo.mcfg
+mpirun --nooversubscribe --hostfile ~/dev/hostfile modular.py correl_demo.mcfg
 #mpirun --nooversubscribe --hostfile ~/dev/hostfile modular.py MM_kinetics_means.mcfg
-mpirun --nooversubscribe --hostfile ~/dev/hostfile modular.py MM_kinetics_fitting.mcfg
+#mpirun --nooversubscribe --hostfile ~/dev/hostfile modular.py MM_kinetics_fitting.mcfg
 echo "finished mpirun of modular!"
 
 
