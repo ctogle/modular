a5f3aaaae6773e01484ddb93239fa373ed23bd1c
ctogle <cogle@vt.edu>
2015-06-04 15:50:20 -0400

    almost working

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dstoolm simulation module',
+    version = '1.0',
+    author = "ctogle",
+    author_email = "cogle@vt.edu",
+    license = "MIT License",
+    packages = ['dstoolm'],
+)
+
+
+
